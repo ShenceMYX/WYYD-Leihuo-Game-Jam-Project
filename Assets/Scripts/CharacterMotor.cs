7bdb7a5c10f12ac3b8ccc5e6cb30b50cfb6ef483
Shence <maoyuxuan129@gmail.com>
2023-04-13 00:31:06 +0800

    Setup the basic scene, import post processing

---
+++ Assets/Scripts/CharacterMotor.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ns
+{
+	/// <summary>
+	///
+	/// </summary>
+	public class CharacterMotor : MonoBehaviour
+	{
+    
+	}
+}
